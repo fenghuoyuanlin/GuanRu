e3640e3a03c2e2eb03a9ab9b84f3c4565b12afda
刘园 <wangyangyang@wangyangyangdeimac.local>
2017-10-23 14:49:38 +0800

    Initial Commit

---
+++ GuanruAppDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GuanruAppDemo
+//
+//  Created by 刘园 on 2017/10/23.
+//  Copyright © 2017年 guanrukeji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
