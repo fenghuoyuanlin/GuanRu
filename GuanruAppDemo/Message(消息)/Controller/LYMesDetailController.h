26ed0c0aec59c4c672945d944fc729ddafad6a86
121057Ab <1210578014@qq.com>
2018-01-12 19:06:46 +0800

    first commit

---
+++ GuanruAppDemo/Message(消息)/Controller/LYMesDetailController.h
@@ -0,0 +1,15 @@
+//
+//  LYMesDetailController.h
+//  GuanruAppDemo
+//
+//  Created by 刘园 on 2018/1/12.
+//  Copyright © 2018年 guanrukeji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LYMesDetailController : UIViewController
+
+@property(nonatomic, strong) NSString *detailStr;
+
+@end
